456a63c70da31b1a2babbe123dfa09935154c481
Yaraslau Dzenisiuk <cylinder.y@gmail.com>
2019-01-17 17:47:04 +0300

    #merge - Fix for #1699 | MCP UI doesn't work because of StackOverflowError exception (#1701)

--- bundle/src/main/java/com/adobe/acs/commons/mcp/impl/ControlledProcessManagerServlet.java
+++ bundle/src/main/java/com/adobe/acs/commons/mcp/impl/ControlledProcessManagerServlet.java
@@ -27,6 +27,7 @@ import com.google.gson.ExclusionStrategy;
 import com.google.gson.FieldAttributes;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+
 import java.io.IOException;
 import java.lang.reflect.Modifier;
 import java.util.ArrayList;
